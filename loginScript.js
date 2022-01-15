95772e0f601f89ef07147a8c98366d8acd50ef19
mymy3910 <mymy3926@gmail.com>
2022-01-15 16:21:34 +0900

    指摘箇所修正・パスワード表示切替追加

--- loginScript.js
+++ loginScript.js
@@ -1,5 +1,6 @@
 //ログインページのログインボタン押下で処理開始
-document.getElementById("login").onclick = function(){
+const login = document.getElementById("login");
+login.addEventListener('click' , function(){
     //会員登録で入力された値の取得
     const gotmail = document.getElementById("gotmail").value;
     const gotpass = document.getElementById("gotpass").value; 
@@ -10,35 +11,24 @@ document.getElementById("login").onclick = function(){
 
     //入力チェック時のエラーメッセージ
     this.errorMessages = [];
-
-    //未入力チェック
-    let flag = 0; 
-    if(mail == ""){
-        flag = 1;
-    }
-    if(pass == ""){
-        flag = 1;
-    }
-
-    if(flag == 1){
-        this.errorMessages.push("未入力の項目があります。");
-    }else{
-        //取得したメールアドレスと入力されたメールアドレスが一致しているか
-        if(gotmail != mail){
-            this.errorMessages.push("入力したメールアドレスが間違っています。");
-        }
-        //取得したパスワードと入力されたパスワードが一致しているか
-        if(gotpass != pass){
-            this.errorMessages.push("入力したパスワードが間違っています。");
-        }
-    }
-
     //エラーメッセージを表示、エラーがなければログイン成功メッセージのみを表示
     if (this.errorMessages.length != 0) { 
         alert(this.errorMessages.join('\n'));
     }else{
         alert("ログインに成功しました！");
-    }   
+    } 
+    //未入力チェック
+    if(mail == ""||pass == ""){
+      this.errorMessages.push("未入力の項目があります。");
+    }
+    //取得したメールアドレスと入力されたメールアドレスが一致しているか
+    if(gotmail != mail){
+        this.errorMessages.push("入力したメールアドレスが間違っています。");
+    }
+    //取得したパスワードと入力されたパスワードが一致しているか
+    if(gotpass != pass){
+        this.errorMessages.push("入力したパスワードが間違っています。");
+    }
+  }, false)
 
-}
 
